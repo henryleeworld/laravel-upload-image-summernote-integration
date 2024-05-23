897b0e701ca6f94a83d50c65f41f0b169f2590a8
Henry Lee <henryleeworld@gmail.com>
2024-05-24 00:11:50 +0800

    feat:升級至 laravel 10

--- resources/views/summernote.blade.php
+++ resources/views/summernote.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css" integrity="sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs5.min.css" integrity="sha512-ngQ4IGzHQ3s/Hh8kMyG4FC74wzitukRMIcTOoKT3EyzFZCILOPF0twiXOQn75eDINUfKBYmzYn2AA8DkAk8veQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <title>Summernote 圖片上傳</title>
+        <title>{{ config('app.name') }}</title>
     </head>
     <body>
         <div class="container">
-            <div class="row">
-                <div class="col-md-8 offset-2 mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8 mt-5">
                     <div class="card">
                         <div class="card-header bg-info">
-                            <h6 class="text-white">Summernote 圖片上傳</h6>
+                            <h6 class="text-white">{{ __('Summernote image upload') }}</h6>
                         </div>
                         <div class="card-body">
                             <form method="post" action="{{ route('summernote.upload') }}" enctype="multipart/form-data">
                                 @csrf
-                                <div class="form-group">
-                                    <label><strong>描述：</strong></label>
+                                <div class="row mb-3">
+                                    <label><strong>{{ __('Description:') }}</strong></label>
                                     <textarea class="form-control summernote" name="description"></textarea>
                                 </div>
-                                <div class="form-group text-center">
-                                    <button type="submit" class="btn btn-success btn-sm">儲存</button>
+                                <div class="mb-0 text-center">
+                                    <button type="submit" class="btn btn-success btn-sm">{{ __('Save') }}</button>
                                 </div>
                             </form>
                         </div>
@@ -29,8 +29,8 @@
                 </div>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js" integrity="sha512-pax4MlgXjHEPfCwcJLQhigY7+N8rt6bVvWLFyUMuxShv170X53TRzGPmPkZmGBhk+jikR8WBM4yl7A9WMHHqvg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js" integrity="sha512-VK2zcvntEufaimc+efOYi622VN5ZacdnufnmX7zIhCPmjhKnOi9ZDMtg1/ug5l183f19gG1/cBstPO4D8N/Img==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs5.min.js" integrity="sha512-6F1RVfnxCprKJmfulcxxym1Dar5FsT/V2jiEUvABiaEiFWoQ8yHvqRM/Slf0qJKiwin6IDQucjXuolCfCKnaJQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/lang/summernote-zh-TW.min.js" integrity="sha512-a57HvLGAsQ3oXr9Y2BGAyGoNVC6kJ8pRKd7A0te9xL1c5MedNd5B/FwtJxRLuaLjSfECzu58z5CvSp6xItnOtQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script type="text/javascript">
@@ -49,7 +49,7 @@
                         ['view', ['fullscreen', 'codeview', 'help']]
                     ],
                     */
-                    lang: 'zh-TW'
+                    lang: '{{ str_replace('_', '-', app()->getLocale()) }}'
                 });
             });
         </script>
